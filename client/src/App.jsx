65502de544c8db2ea669c3b5bf0d784235ed8681
Sairoden <odingandarosa@gmail.com>
2023-11-30 20:56:10 +0800

    Add Routes and Header UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,30 @@
+// LIBRARIES
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// PAGES
+import { Home, CreatePost } from "./pages";
+
+// UI COMPONENTS
+import { Header } from "./ui";
+
 function App() {
-  return <h1>Hello World</h1>;
+  const router = createBrowserRouter([
+    {
+      element: <Header />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/create-post",
+          element: <CreatePost />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
